fd92b3162d864ca50fb772cdd7136a2767bc6c33
arushi-kalra <41520590+arushi-kalra@users.noreply.github.com>
2020-04-19 19:39:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+employee_info={
+	"emp1":
+	{
+	"name":"xyz",
+	"sal":"10"
+	},
+	"emp2":
+	{
+	"name":"abc",
+	"sal":"14"
+	}
+}
+@app.route('/predict',methods=['GET'])
+def predict():
+	return employee_info
+
+app.run(debug=True)
